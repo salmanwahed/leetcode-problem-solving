5d5ecbadd5877c3f2d578f1a4dccef17da0e15b6
Salman Wahed <dev.salmanwahed@gmail.com>
2024-09-06 20:26:26 +0600

    - [Sliding Window] Maximum number of vowels in a substring of given length.

--- 1456_maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456_maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,8 +1,11 @@
 class Solution:
+    def __init__(self):
+        self.vowels = {'a', 'e', 'i', 'o', 'u'}
+
     def __count_vowel(self, st):
         count = 0
         for ch in st:
-            if ch in "aeiou":
+            if ch in self.vowels:
                 count += 1
         return count
 
@@ -11,15 +14,16 @@ class Solution:
         vc = max_vc = self.__count_vowel(s[left:right])
 
         while right < len(s):
-            if s[left] in "aeiou":
+            if s[left] in self.vowels:
                 vc -= 1
-            if s[right] in "aeiou":
+            if s[right] in self.vowels:
                 vc += 1
             max_vc = max(max_vc, vc)
             left += 1
             right += 1
 
         return max_vc
+
 
 if __name__ == '__main__':
     sol = Solution()
